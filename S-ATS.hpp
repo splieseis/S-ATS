577267a36f9c57bcfa42a4fcee687b97a1a3506e
splieseis <s.plieseis@hotmail.com>
2019-02-07 02:11:05 +0100

    function prototypes added

---
+++ S-ATS.hpp
@@ -0,0 +1,12 @@
+#ifndef S_ATS_HPP
+# define S_ATS_HPP
+
+/* Function Prototypes */
+
+void printHeader();
+void imprint();
+void details(vector <Applicant> candidates, int i);
+void listCandidates(vector <Applicant> candidates);
+Applicant newCandidate(void);
+
+#endif
